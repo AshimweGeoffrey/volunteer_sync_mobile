6644e549366a36ad18e6a49a268e1486bda4ba74
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2025-02-19 18:10:02 +0200

    Add: Implemented Google auth screen

---
+++ lib/widgets/google_sign_in_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        primary: Colors.white,
+        onPrimary: Colors.black,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/google_logo.png', height: 20),
+          SizedBox(width: 10),
+          Text('Sign in with Google'),
+        ],
+      ),
+    );
+  }
+}
